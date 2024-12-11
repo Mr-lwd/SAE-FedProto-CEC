7dae94918186138b5a235530dcf4719487a263ed
Mr-lwd <1850245174@qq.com>
2024-12-11 09:35:39 +0800

    MNIST SAE

--- system/main.py
+++ system/main.py
@@ -312,28 +312,30 @@ if __name__ == "__main__":
         "-dev", "--device", type=str, default="cuda", choices=["cpu", "cuda"]
     )
     parser.add_argument("-did", "--device_id", type=str, default="0")
+    parser.add_argument(
+        "-data", "--dataset", type=str, default="MNIST_dir_0.3_imbalance_40"
+    )
     # parser.add_argument(
     #     "-data", "--dataset", type=str, default="FashionMNIST_dir_0.3_imbalance_40"
     # )
-    parser.add_argument(
-        "-data", "--dataset", type=str, default="Cifar10_dir_0.3_imbalance_40"
-    )
+    # parser.add_argument(
+    #     "-data", "--dataset", type=str, default="Cifar10_dir_0.3_imbalance_40"
+    # )
     parser.add_argument("-nb", "--num_classes", type=int, default=10)
-    # parser.add_argument("-m", "--model_family", type=str, default="HCNNs8")
-    parser.add_argument("-m", "--model_family", type=str, default="HtM10")
+    parser.add_argument("-m", "--model_family", type=str, default="HCNNs8")
+    # parser.add_argument("-m", "--model_family", type=str, default="HtM10")
     parser.add_argument("-lbs", "--batch_size", type=int, default=256)
     parser.add_argument(
         "-lr",
         "--local_learning_rate",
         type=float,
-        # default=0.05,
         default=0.06,
         help="Local learning rate",
     )
     parser.add_argument("-usche", "--use_decay_scheduler", type=bool, default=False)
     parser.add_argument("-ld", "--learning_rate_decay", type=bool, default=False)
     parser.add_argument("-ldg", "--learning_rate_decay_gamma", type=float, default=0.99)
-    parser.add_argument("-gr", "--global_rounds", type=int, default=110)
+    parser.add_argument("-gr", "--global_rounds", type=int, default=100)
     parser.add_argument(
         "-edge_epochs", "--edge_epochs", type=int, default=1, help="edge epoches"
     )
--- system/total.sh
+++ system/total.sh
@@ -3,4 +3,8 @@
 
 
 # nohup python -u main.py > ./logs/Cifar10/Cifar10_dir_0.3_imbalance_40/epochs_3/fedproto_lr006.out 2>&1 &
-nohup python -u main.py > ./logs/Cifar10/Cifar10_dir_0.3_imbalance_40/epochs_3/SAE_gamma_1.0_buffer=0.5_joinrate_1.0.out 2>&1 &
+# nohup python -u main.py > ./logs/Cifar10/Cifar10_dir_0.3_imbalance_40/epochs_3/SAE_gamma_1.0_buffer=0.5_joinrate_1.0.out 2>&1 &
+
+
+nohup python -u main.py > ./logs/MNIST/MNIST_dir_0.3_imbalance_40/epochs_3/fedproto.out 2>&1 &
+# nohup python -u main.py > ./logs/MNIST/MNIST_dir_0.3_imbalance_40/epochs_3/SAE_gamma_1.0_buffer=0.5_joinrate_1.0.out 2>&1 &
