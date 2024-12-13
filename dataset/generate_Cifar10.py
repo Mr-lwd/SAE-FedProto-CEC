8fb35f1f10c93b804603892f286a145bc02065f8
Mr-lwd <1850245174@qq.com>
2024-12-13 09:42:06 +0800

    gendataset

---
+++ dataset/generate_Cifar10.py
@@ -0,0 +1,97 @@
+# PFLlib: Personalized Federated Learning Algorithm Library
+# Copyright (C) 2021  Jianqing Zhang
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import numpy as np
+import os
+import sys
+import random
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from utils.dataset_utils import check, separate_data, split_data, save_file
+
+from args import args_parser
+
+# Allocate data to users
+def generate_dataset(niid, balance, partition, args):
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    num_clients = args.num_users
+    dir_path = f"Cifar10_{args.partition}_{args.alpha if args.partition == 'dir' else args.class_per_client}_{args.balance}_{args.num_users}/"
+
+
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+        
+    # Setup directory for train/test data
+    config_path = dir_path + "config.json"
+    train_path = dir_path + "train/"
+    test_path = dir_path + "test/"
+
+    if check(config_path, train_path, test_path, num_clients, niid, balance, partition):
+        return
+        
+    # Get Cifar10 data
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.CIFAR10(
+        root="./", train=True, download=True, transform=transform)
+    testset = torchvision.datasets.CIFAR10(
+        root="./", train=False, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=len(trainset.data), shuffle=False)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=len(testset.data), shuffle=False)
+
+    for _, train_data in enumerate(trainloader, 0):
+        trainset.data, trainset.targets = train_data
+    for _, test_data in enumerate(testloader, 0):
+        testset.data, testset.targets = test_data
+
+    dataset_image = []
+    dataset_label = []
+
+    dataset_image.extend(trainset.data.cpu().detach().numpy())
+    dataset_image.extend(testset.data.cpu().detach().numpy())
+    dataset_label.extend(trainset.targets.cpu().detach().numpy())
+    dataset_label.extend(testset.targets.cpu().detach().numpy())
+    dataset_image = np.array(dataset_image)
+    dataset_label = np.array(dataset_label)
+
+    num_classes = len(set(dataset_label))
+    print(f'Number of classes: {num_classes}')
+
+    # dataset = []
+    # for i in range(num_classes):
+    #     idx = dataset_label == i
+    #     dataset.append(dataset_image[idx])
+
+    X, y, statistic = separate_data((dataset_image, dataset_label), num_clients, num_classes,  
+                                    niid, balance, partition, alpha=args.alpha, class_per_client=args.class_per_client)
+    train_data, test_data = split_data(X, y)
+    save_file(config_path, train_path, test_path, train_data, test_data, num_clients, num_classes, 
+        statistic, niid, balance, partition, args.alpha, class_per_client=args.class_per_client)
+
+
+if __name__ == "__main__":
+    args = args_parser()
+    niid = True if args.niid == "noniid" else False
+    balance = True if args.balance == "balance" else False
+    partition = args.partition if args.partition != "-" else None
+
+    generate_dataset(niid, balance, partition, args)
