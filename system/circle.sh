7c118629dbd90082f8f49caa22d289725c54cdaa
Mr-lwd <1850245174@qq.com>
2024-12-14 09:47:03 +0800

    FashionMNIST

--- system/circle.sh
+++ system/circle.sh
@@ -1,3 +1,3 @@
-nohup python -u main.py -algo FedSAE -data FashionMNIST_dir_0.3_imbalance_40 -gr 400 -nc 40 -lr 0.06 > ./logs/FashionMNIST/FashionMNIST_dir_0.3_imbalance_40/epochs_3/SAE_addTGP_gamma_1.0_buffer=0.5_joinrate_1.0_test.out 2>&1 &
+# nohup python -u main.py -algo FedSAE -data FashionMNIST_dir_0.3_imbalance_40 -gr 400 -nc 40 -lr 0.06 > ./logs/FashionMNIST/FashionMNIST_dir_0.3_imbalance_40/epochs_3/SAE_addTGP_gamma_1.0_buffer=0.5_joinrate_1.0_test.out 2>&1 &
 
-nohup python -u main.py -algo FedSAE -data FashionMNIST_dir_0.3_imbalance_40 -gr 400 -nc 40 -lr 0.06 -addTGP 0 > ./logs/FashionMNIST/FashionMNIST_dir_0.3_imbalance_40/epochs_3/SAE_gamma_1.0_buffer=0.5_joinrate_1.0.out 2>&1 &
+# nohup python -u main.py -algo FedSAE -data FashionMNIST_dir_0.3_imbalance_40 -gr 400 -nc 40 -lr 0.06 -addTGP 0 > ./logs/FashionMNIST/FashionMNIST_dir_0.3_imbalance_40/epochs_3/SAE_gamma_1.0_buffer=0.5_joinrate_1.0.out 2>&1 &
