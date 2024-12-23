3f0a7d3472e1a3632df51c7c0f1e32e0b12e87a8
Mr-lwd <1850245174@qq.com>
2024-12-23 21:48:32 +0800

    fix plot

--- system/logs/Cifar10/Cifar10_dir_0.3_imbalance_40/epochs_10/energy&acc.py
+++ system/logs/Cifar10/Cifar10_dir_0.3_imbalance_40/epochs_10/energy&acc.py
@@ -48,11 +48,7 @@ def extract_loss_data(log_file):
 
 # 定义文件路径
 log_files = [
-    "./SGD_lr_006_lam_1_momentum_0.5_batch_256_FedProto.out",
-    # "./SGD_lr_008_lam_2_momentum_0_batch_256_FedProto.out",
-    # "./lr_006_mo_0.5_lam_1_batch_256_FedSAE_gam_0.7.out",
-    "./lr_006_mo_0.5_lam_1_batch_256_FedSAE_gam_1.out",
-    "./SGD_lr_006_mo_0.5_lam_1_batch_256_FedTGP.out"
+    "./lr_008_mo_0_lam_2_batch_256_FedSAE_gam_1.out"
 ]
 
 time_model_configs = [ 
